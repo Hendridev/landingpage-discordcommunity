7c0c19ba89d0eb5778ab019719a572048354f4aa
Hendridev <hendridevlp@gmail.com>
2021-05-28 22:07:25 +0700

    transition

--- public/js/script.js
+++ public/js/script.js
@@ -1,5 +1,9 @@
 $(document).ready(function() {
     $('.dropdown').on('click', function() {
         $(this).parent().next().toggleClass('hidden');
+        let el = $(this);
+        window.setTimeout(function() {
+            el.parent().next().toggleClass('opacity-100');
+        }, 200);
     })
 });
